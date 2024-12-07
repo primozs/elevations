5516ef37a574ca88923eb586f82acb276f1fd053
Primoz Susa <primoz.susa@pm.me>
2024-12-07 11:17:30 +0100

    cache

--- src/elevations.nim
+++ src/elevations.nim
@@ -5,19 +5,21 @@ import std/math
 import std/strutils
 import std/asyncdispatch
 import std/httpclient
+import pkg/lrucache
 import ./hgt
 
 
 type HgtTileset* = object
-  cache*: int = 128
+  cache*: LruCache[string, int]
   pwd*: string = "skadi"
 
 
 const S3Url = "https://elevation-tiles-prod.s3.amazonaws.com/skadi"
 
 
-proc initHgtTileset*(dir: string): HgtTileset {.raises: [].} =
-  let tileset = HgtTileset(pwd: dir)
+proc initHgtTileset*(dir: string, cacheSize: int = 1000000): HgtTileset {.raises: [].} =
+  var tileset = HgtTileset(pwd: dir)
+  tileset.cache = newLRUCache[string, int](cacheSize)
   return tileset
 
 
@@ -62,10 +64,28 @@ proc getTile(ts: HgtTileset, lat, lng: float): Future[string] {.async.} =
     file.close()
 
 
+proc cacheKey(lat, lon: float): string =
+  fmt"{lat}{lon}"
+
+
+template checkCache(ts: HgtTileset, lat, lon: float) =
+  let key = cacheKey(lat, lon)
+  if key in ts.cache:
+    echo "has cache sync"
+    return ts.cache[key]
+
+
 proc getElevation*(tileset: HgtTileset, lat, lon: float): int {.raises: [].} =
   try:
+    let key = cacheKey(lat, lon)
+    if key in tileset.cache:
+      return tileset.cache[key]
+
     let tilePath = waitFor tileset.getTile(lat, lon)
-    return tilePath.hgtGetElevation(lat, lon)
+    let ele = tilePath.hgtGetElevation(lat, lon)
+
+    tileset.cache[key] = ele
+    return ele
   except Exception as e:
     echo e.repr
     return -32768
@@ -74,8 +94,16 @@ proc getElevation*(tileset: HgtTileset, lat, lon: float): int {.raises: [].} =
 proc getElevationAsync*(tileset: HgtTileset, lat, lon: float): Future[
     int] {.async.} =
   try:
+    let key = cacheKey(lat, lon)
+    if key in tileset.cache:
+      return tileset.cache[key]
+
+    # checkCache(tileset, lat, lon)
     let tilePath = await tileset.getTile(lat, lon)
-    return tilePath.hgtGetElevation(lat, lon)
+    let ele = tilePath.hgtGetElevation(lat, lon)
+
+    tileset.cache[key] = ele
+    return ele
   except Exception as e:
     echo e.repr
     return -32768
@@ -91,6 +119,7 @@ proc getElevation*(tileset: HgtTileset, locations: seq[seq[float]]): seq[
     result = waitFor all(results)
   except Exception as e:
     echo e.repr
+
 
 when isMainModule:
   assert getTileFilePath(51.3, 13.4) == "N51/N51E013.hgt.gz"
